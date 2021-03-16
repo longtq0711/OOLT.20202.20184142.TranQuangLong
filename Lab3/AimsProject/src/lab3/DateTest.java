18249a4597c077899e3a5a373d29fe95091d5662
Long Tran Quang <longtq0711@gmail.com>
2021-03-16 20:08:22 +0700

    Update

---
+++ Lab3/AimsProject/src/lab3/DateTest.java
@@ -0,0 +1,20 @@
+package com.example.main;
+
+public class DateTest {
+	public static void main(String[] args)throws Exception {
+		MyDate date1 = new MyDate("February 18th 2000");
+		date1.print();
+		MyDate date2 = new MyDate();
+		date2.print();
+		MyDate date3 = new MyDate(7,11,2000);
+		date3.print(); 
+		MyDate date4 = new MyDate();
+		date4.setDay(5);
+		date4.setMonth(9);
+		date4.setYear(2021);
+		date4.print();
+		date3.accept();
+		date3.print();
+	}
+
+}
